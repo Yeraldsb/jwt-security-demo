8619aef332973bab3ed877bd1ab0f8d21852c4fa
YeraldSb <pamelasb9412@gmail.com>
2025-04-25 08:17:59 +0200

    feat: complete JWT demo with final improvements

--- src/index.js
+++ src/index.js
@@ -7,50 +7,45 @@ const app = express();
 app.use(express.json());
 
 app.post("/login", (req, res) => {
-    //curl -X POST http://localhost:3000/login -H "Content-Type: application/json" -d '{"username":"admin","password":"admin"}'
+    //Curl -X POST http://localhost:3000/login -H "Content-Type: application/json" -d '{"username":"user","password":"user"}'
 
   const { username, password  } = req.body;
 
 
   if (username === "user" && password === "user") {
     return res.json({
-        token: jwt.sign({ username: "user" }, SECRET),
+        token: jwt.sign({ userid: "user" }, SECRET),
     });
   }
 
   if (username === "admin" && password === "admin") {
     return res.json({
-        token: jwt.sign({ username: "admin" }, SECRET),
+        token: jwt.sign({ userid: "admin" }, SECRET),
     });
   }
   return res
   .status(401)
   .json({
-    message: "Username or password is invalid",    
-  });
-
-  app.get("/dashboard", (req, res) => {
-    //curl -i localhost/dashboard --Headder 'Authorization: Bearer <token>'
-    const token = req.headers.authorization.split(' ')[1];
-    if (!token) {
-        return res.status(401).json({
-            message: "Token is missing",
-        });
-        try {   
-            const decoded = jwt.verify(token, SECRET);
-            req.user = decoded;
-        }
-        catch (e) {
-            res.status(401).json(e)
-        }
-
-        return res.status(200).json({message: `Welcome to the dashboard" ${req.user.username}`}); 
-    }
- 
-  })  
+    message: "Username or password is invalid"});
+});
   
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
+app.get("/dashboard", (req, res) => {
+  //curl -i http://localhost/dashboard --header 'Authorization: Bearer <token>'
+  const token = req.headers.authorization.split(' ')[1];
+  if (!token) return res.status(401).json('unauthorized');
+      try {   
+          const decoded = jwt.decode(token, SECRET);
+          req.user = decoded;
+      }
+      catch (e) {
+          res.status(401).json(e)
+      }
 
-})
+      return res.status(200).json({message: `Welcome to the dashboard" ${req.user.userid}`}); 
+  
+
+})  
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
